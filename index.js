9b10c1927c0767244433b64cf05a32bf32f674b3
Abir Taheer <abir@taheer.me>
2021-09-04 01:54:14 -0400

    completed authentication procedure

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+
+const puppeteer = require('puppeteer');
+const URL = require("url").URL;
+
+if(! process.env.HUNTERNET_USERNAME || !process.env.HUNTERNET_PASSWORD){
+  throw new Error("You must set the HUNTERNET_USERNAME and HUNTERNET_PASSWORD environment variables before running this app.");
+}
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const handleLogin = async  () =>
+{
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://neverssl.com/', {waitUntil: 'networkidle2'});
+  await sleep(5000);
+  const url = new URL(page.url());
+
+  if(url.hostname === "neverssl.com"){
+    // Nothing needs to be done, the request succeeded
+    await page.close();
+    await browser.close();
+    return;
+  }
+
+  await page.waitForSelector("form>input");
+  await page.click("#user");
+  await page.type("#user", process.env.HUNTERNET_USERNAME);
+
+  await page.click("#password");
+  await page.type("#password", process.env.HUNTERNET_PASSWORD);
+
+  await page.click('input[type=\"submit\"]');
+  await page.waitForNavigation();
+  console.log("Completed Authentication on " + new Date().toString());
+}
+
+setTimeout(handleLogin, 1000 * 60 * 2);
